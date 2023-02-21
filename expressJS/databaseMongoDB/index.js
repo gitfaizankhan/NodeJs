d70ea2a33ebda0a3beabfd52d5a3e301e56d695b
gitfaizankhan <faizankhaninfo9@gmail.com>
2023-02-21 16:20:29 +0530

    connection in s file

--- expressJS/databaseMongoDB/index.js
+++ expressJS/databaseMongoDB/index.js
@@ -1,15 +1,31 @@
-const {MongoClient} = require('mongodb')
-const url = 'mongodb://0.0.0.0:27017/';
-const database = 'nodejs';
-const client = new MongoClient(url);
+// const {MongoClient} = require('mongodb')
+// const url = 'mongodb://0.0.0.0:27017/';
+// const database = 'nodejs';
+// const client = new MongoClient(url);
 
-async function getData(){
-  let result = await client.connect();
-  db = result.db(database);
-  collection = db.collection('node')
-  let data = await collection.find({}).toArray();
+// async function dbConnect(){
+//   let result = await client.connect();
+//   db = result.db(database);
+//   return db.collection('node')
+//   // let data = await collection.find({}).toArray();
+//   // console.log(data);
+// }
+
+
+// dbConnect().then((response)=>{
+//   response.find().toArray().then((data)=>{
+//     console.log(data)
+//   })
+// })
+
+
+const dbConnect = require('./mongodb');
+
+const main = async ()=>{
+  console.log("Main Function Called")
+  let data = await dbConnect()
+  data = await data.find().toArray();
   console.log(data);
 }
 
-
-getData();
+main();

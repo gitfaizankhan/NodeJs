e6f836a497ef18bd3868391c72b0ff755b1f6663
gitfaizankhan <faizankhaninfo9@gmail.com>
2023-02-24 17:31:07 +0530

    mongoose api

---
+++ expressJS/mongooseApi/mongooseDb.js
@@ -0,0 +1,8 @@
+// const mongoose = require('mongoose');
+
+// const connection = async ()=>{
+//     await mongoose.connect("mongodb://0.0.0.0:27017/e-com");
+    
+// }
+
+// module.exports = connection;

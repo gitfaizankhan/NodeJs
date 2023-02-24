e176cb60a031f9887475fe9dde253d7ccf89b9e5
gitfaizankhan <faizankhaninfo9@gmail.com>
2023-02-24 15:22:05 +0530

    delete api

--- expressJS/API/curdAPI.js
+++ expressJS/API/curdAPI.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const connection = require('./mongoConnection');
-
+const mongoDb = require('mongodb');
 
 const app = express();
-
-
+app.use(express.json());
+// get api method
 app.get('/', async  (request, response)=>{
     // response.send("Hello Faizan Khan");
     let data = await connection();
@@ -14,5 +14,35 @@ app.get('/', async  (request, response)=>{
 })
 
 
+
+// post method
+app.post('/', async (request, response)=>{
+    console.log(request.body);
+    // console.log(response.send(request.body));
+    let data = await connection();
+    let insertdata = await data.insertOne(request.body);
+    response.send(insertdata);
+
+})
+
+
+// put method
+
+app.put('/', async (request, response)=>{
+    let data = await connection();
+    let updatedData = await data.updateOne(
+        {brand:request.body.brand},
+        {$set:request.body}
+    )
+    response.send(updatedData);
+})
+
+
+// delete method
+app.delete('/:id', async (request, response)=>{
+    let data = await connection();
+    let deleteItem = await data.deleteOne({_id: new mongoDb.ObjectId(request.params.id)});
+    response.send(deleteItem);
+})
 
 app.listen(5000, console.log("Server running on 5000"));
